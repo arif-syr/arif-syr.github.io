2bfab4afa80cb6d096f05160677a1c9f29d803e2
Arif Syraj <126988341+arif-syr@users.noreply.github.com>
2024-02-05 20:39:47 -0800

    Fix visual bug

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Arif\'s github. //<code>src/App.js</code>
+          Welcome to Arif's github.
         </p>
         <a
           className="App-link"
