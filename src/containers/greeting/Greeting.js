be31739d3ebd9f085c09931b569b4705576cd2c9
Arif Syraj <138091471+arif-syraj-usfca@users.noreply.github.com>
2025-01-11 16:00:33 +0400

    Add resume

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,7 +42,7 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
+                    href="https://github.com/arif-syr/resume_repository/raw/refs/heads/master/Arif-Syraj-resume.pdf"
                     download="Resume.pdf"
                     className="download-link-button"
                   >
